9851c8c11da728c48a811ddd58e078ed44f450c2
Vikram Dulani <soul.id.app@gmail.com>
2020-11-24 18:10:46 +0530

    Initial commit

--- deploy/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Running master web"
-docker stop soul.technology || true && docker rm soul.technology || true
-docker run -d -p 80:80 --name soul.technology registry.github.com/soul-technology-labs/soul.technology:v0.1
